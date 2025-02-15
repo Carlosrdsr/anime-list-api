e992be8a35289a011b3eb6523efe4963e652db2f
Carlosrdsr <carlosrdsr@gmail.com>
2025-02-15 19:02:56 -0300

    add script create table

--- create-migration.sh
+++
@@ -1,3 +0,0 @@
-export ASPNETCORE_ENVIRONMENT=Development
-
-dotnet ef -s src/AnimeList.API migrations add AddTableAnime -c "AnimeDbContext" -p src/AnimeList.Infrastructure/AnimeList.Infrastructure.csproj
